ad6aae9a1e9f1940b6c7204c4bdca3baf3d081d2
sunny <sunnykushwaha07@gmail.com>
2024-02-08 08:02:00 +0530

    add view page of property and set root page

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,6 @@
+class HomesController < ApplicationController
+	skip_before_action :authenticate_request
+	def index
+		render 'home/index'
+	end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
  
-  root "logins#new"
+  root "homes#index"
+
+  resources :homes, only: [:index]
   resources :logins, only: [:new]
   namespace :logins do
     post 'login'
   end
-
-  resources :registrations, only: [:create,:index]
+  resources :registrations, only: [:create,:index,:new]
   resources :users, only: [:index]
-  resources :properties
+  resources :properties do
+    collection do
+      get 'search_property'
+    end
+  end
   resources :favorites, only: [:create, :destroy]
 end
